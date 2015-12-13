17618347c3758c70df480bd898f9b4ed27e9ede9
xtfkpi <vitaliy@brightlocal.com>
2015-12-13 12:16:45 +0200

    sometimes works for two users

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
 var counter = 0;
 var id = 0;
-var maxPlayers = 1;
+var maxPlayers = 2;
 var score1 = 0, score2 = 0;
 var myFirebaseRef = new Firebase("https://boiling-torch-1773.firebaseio.com//");
 var testBase = [["what language can be used for writing backend?", "java", "html", "chinese", 1],
+    ["Кого зовут на новый год?","Дедушку мороза","Линуса Торвальдса","Бабу Ягу",1],
+    ["Для чего нужен бубен?","Для настройки серверов","Для улучшения приема wi-fi","Для игры в ансамблях",3],
+    ["Кто отвечает за саппорт на Hack new year","Александра Поляничко","Анастасия Лужочко","Алина Степочко",1],
     ["What program can be used to surf Internet?", "firefox", "notepad", "disk defragmemnator", 1],
     ["Which of items is an operation system?", "Minisoft Clismows", "Microsoft Windows", "RedCab Lanux", 2]
 ];
 myFirebaseRef.child("questionItem").on("value", function (snapshot) {
     console.log('something updated on server ' + snapshot.val().question);
-    main.answer.value = snapshot.val().question;
+    //main.answer.value = snapshot.val().question;
+    document.getElementById("question").innerHTML=snapshot.val().question;
     main.b1.value = snapshot.val().answer1;
     main.b2.value = snapshot.val().answer2;
     main.b3.value = snapshot.val().answer3;
@@ -20,6 +24,7 @@ myFirebaseRef.child("resetGame").on("value", function (snapshot) {
     id = 0;
     console.log('game ended');
 });
+
 
 function foo() {
     console.log("reset");
@@ -67,11 +72,15 @@ function bar() {
                         if (turn1 === answer) {
                             console.log('player1 is amazing smart');
                             score1 += 1;
+
                         }
                         if (turn2 === answer) {
                             console.log('player2 is amazing smart');
                             score2 += 1;
+
                         }
+                        document.getElementById("score1").innerHTML=score1;
+                        document.getElementById("score2").innerHTML=score2;
                         if (counter < testBase.length - 1) {
                             counter += 1;
                             nextQuestion();
